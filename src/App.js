e749e3ee07b613caa7a364cb8d48836267eb5f31
huy nguyen <huynguyen@huys-MacBook-Pro.local>
2020-08-18 15:36:29 -0500

    Edit package.json add name property

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.page';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
 }
 
